677f583ca812e8bb6dd3a4f2cafdaf193346df6e
hvhtgg8-design <hvhtgg8@gmail.com>
2025-12-14 17:19:28 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -2,23 +2,19 @@
 SET APPDATA_DIR=%LOCALAPPDATA%\delpa_for_bus
 IF NOT EXIST "%APPDATA_DIR%" mkdir "%APPDATA_DIR%"
 
-:: Move existing files if they exist in Downloads, Desktop, or C:\
-IF EXIST "%USERPROFILE%\Downloads\main.py" copy "%USERPROFILE%\Downloads\main.py" "%APPDATA_DIR%\main.py"
-IF EXIST "%USERPROFILE%\Desktop\main.py" copy "%USERPROFILE%\Desktop\main.py" "%APPDATA_DIR%\main.py"
-IF EXIST "C:\main.py" copy "C:\main.py" "%APPDATA_DIR%\main.py"
+REM Download files if they don't exist in the APPDATA folder
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/hvhtgg8-design/delpa-for-bus/refs/heads/main/main.py' -OutFile '%APPDATA_DIR%\main.py'"
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/hvhtgg8-design/delpa-for-bus/refs/heads/main/oauth_client.py' -OutFile '%APPDATA_DIR%\oauth_client.py'"
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/hvhtgg8-design/delpa-for-bus/refs/heads/main/code_it_for_it.py' -OutFile '%APPDATA_DIR%\code_it_for_it.py'"
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/hvhtgg8-design/delpa-for-bus/refs/heads/main/make_config.py' -OutFile '%APPDATA_DIR%\make_config.py'"
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/hvhtgg8-design/delpa-for-bus/refs/heads/main/encoded_secrets.json' -OutFile '%APPDATA_DIR%\encoded_secrets.json'"
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/hvhtgg8-design/delpa-for-bus/refs/heads/main/server.js' -OutFile '%APPDATA_DIR%\server.js'"
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/hvhtgg8-design/delpa-for-bus/refs/heads/main/LICENSE' -OutFile '%APPDATA_DIR%\LICENSE'"
 
-IF EXIST "%USERPROFILE%\Downloads\oauth_client.py" copy "%USERPROFILE%\Downloads\oauth_client.py" "%APPDATA_DIR%\oauth_client.py"
-IF EXIST "%USERPROFILE%\Desktop\oauth_client.py" copy "%USERPROFILE%\Desktop\oauth_client.py" "%APPDATA_DIR%\oauth_client.py"
-IF EXIST "C:\oauth_client.py" copy "C:\oauth_client.py" "%APPDATA_DIR%\oauth_client.py"
-
-IF NOT EXIST "%APPDATA_DIR%\main.py" powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/YOUR_USER/YOUR_REPO/main.py' -OutFile '%APPDATA_DIR%\main.py'"
-IF NOT EXIST "%APPDATA_DIR%\oauth_client.py" powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/YOUR_USER/YOUR_REPO/oauth_client.py' -OutFile '%APPDATA_DIR%\oauth_client.py'"
-IF NOT EXIST "%APPDATA_DIR%\encoded_secrets.json" powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/YOUR_USER/YOUR_REPO/encoded_secrets.json' -OutFile '%APPDATA_DIR%\encoded_secrets.json'"
-
-:: Create run_main.bat on Desktop
+REM Create a desktop shortcut/batch to run the app as admin
 echo @echo off > "%USERPROFILE%\Desktop\run_main.bat"
 echo cd /d "%APPDATA_DIR%" >> "%USERPROFILE%\Desktop\run_main.bat"
 echo powershell -Command "Start-Process python main.py -Verb runAs" >> "%USERPROFILE%\Desktop\run_main.bat"
 
-echo Setup completed!
+echo Setup completed! All files are in %APPDATA_DIR%.
 pause
